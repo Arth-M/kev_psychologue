422ba83074751605dde00bab1654612816c3141c
Arthur-Henri Michalland <ah.michalland@gmail.com>
2025-11-28 17:51:41 +0100

    in progress

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Next.js!</h1>
+    <div className="mt-30 mask-b-to-50%">
+      <h1 className="bg-danube-600">Welcome to Next.js!</h1>
       <p>Your app is running successfully.</p>
       <div className="h-90 w-screen bg-danube-400/70"></div>
       <div className="h-90 w-screen bg-mantis-400/60"></div>
