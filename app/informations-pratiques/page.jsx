4d7725981d6bd39b0957408fe06f6bd66fe945cf
Arthur-Henri Michalland <ah.michalland@gmail.com>
2026-01-09 11:53:48 +0100

    consultation ok responsive

--- app/informations-pratiques/page.jsx
+++ app/informations-pratiques/page.jsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function InformationsPratiques() {
   return (
     <div id="section-1">
-          <div className="absolute background-bureau top-60 md:top-60 md:left-5 md:w-1/2 w-full h-90 border border-gray-300"></div>
-          <div className="absolute background-bureau-2 top-100 md:top-118 md:left-130 md:w-1/2 w-full h-90 border border-gray-200"></div>
-      <div className="mt-35 mb-85 px-5 relative flex flex-wrap items-center justify-center md:space-x-25 space-x-10">
+          <div className="absolute background-bureau top-70 md:top-70 lg:left-5 md:left-0 md:w-1/2 w-4/5 h-90 border border-gray-300"></div>
+          <div className="absolute background-bureau-2 top-200 right-0 md:top-128 md:right-0 lg:left-130 md:w-1/2 w-4/5 h-90 border border-gray-200"></div>
+      <div className="mt-45 mb-85 px-5 relative flex flex-wrap items-center justify-center md:space-x-25 space-x-10">
         <div className="border border-gray-300 min-w-sm px-10 py-5 rounded-2xl bg-white/85">
           <div className="grid grid-cols-[minmax(45px,1fr)_minmax(120px,5fr)] items-center mt-5">
             <Image
@@ -75,7 +75,7 @@ export default function InformationsPratiques() {
           </div>
         </div>
 
-        <div className="mt-25 ml-10 sm:mt-25 sm:ml-35 md:mt-0 md:ml-0 shadow-xl">
+        <div className="mt-25 ml-10 sm:mt-25 sm:ml-35 md:mt-25 md:ml-0 lg:mt-0 shadow-xl sm:w-fit">
           <iframe id="#maps" className="mx-auto rounded-lg" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2886.531190821487!2d4.008853675934832!3d43.65792125212588!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6a138c4189c73%3A0xc36827070fa392fb!2sKevin%20Nicolas!5e0!3m2!1sfr!2sfr!4v1765793904755!5m2!1sfr!2sfr" width="400" height="300" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" alt="Plan de la localisation du cabinet"></iframe>
         </div>
       </div>
