0de7118e0d71fdd5f39ceb0eadf36e5e6b0194b5
Arthur-Henri Michalland <ah.michalland@gmail.com>
2026-01-27 13:30:10 +0100

    added movement

--- app/consultations/page.jsx
+++ app/consultations/page.jsx
@@ -34,14 +34,22 @@ export default function Consultations() {
         </h1>
         <hr className="w-25 text-white md:ml-30 ml-4" />
       </div>
-      <div className="absolute background-bureau top-70 md:top-60 lg:right-5 md:right-0 md:w-1/2 w-4/5 h-90 border border-gray-300 to-show animate-on-scroll"></div>
-      <div className="absolute background-bureau-2 top-200 right-0 md:top-142 md:left-0 lg:left-5 md:w-1/2 w-4/5 h-90 border border-gray-200 to-show animate-on-scroll"></div>
+      <div className="absolute background-bureau top-70 md:top-60 lg:right-5 md:right-0 md:w-1/2 w-4/5 h-90 to-show animate-on-scroll"></div>
+      <div className="absolute background-bureau-2 top-200 right-0 md:top-142 md:left-0 lg:left-5 md:w-1/2 w-4/5 h-90 to-show animate-on-scroll"></div>
       <div className="pt-35 relative to-show animate-on-scroll">
         <div id="infos-pratiques" className="scroll-mt-30 flex flex-nowrap w-full pl-25 mb-5 z-90 text-lg to-show">
-          <h2 className="rounded-l px-3 py-1 text-slate-100">Informations pratiques</h2>
-          <span className="px-3 py-1 text-gray-300">-</span>
-          <a href="#pour-qui" className="hover:scale-105 hover:animate-pulse">
-            <h2 className="rounded-r px-3 py-1 text-gray-300">Pour qui </h2>
+          <h2 className="rounded-l px-3 py-1 text-white">Informations pratiques</h2>
+          {/* <span className="px-3 py-1 text-gray-300">-</span> */}
+          <a href="#pour-qui" className="group hover:scale-104">
+            <h2 className="rounded-r px-3 py-1 text-gray-300 group-hover:text-gray-200">Pour qui </h2>
+            <hr
+              className="w-fit border-t
+              border-gray-200
+                -mt-1.5
+                mx-auto
+                group-hover:animate-[hr-fill_0.8s_cubic-bezier(0.4,0,0.2,1)_forwards]
+                group-hover:w-[75%]"
+            />
           </a>
         </div>
       <div className="relative mb-20 sm:mb-15 sm:px-25 px-5 flex items-center justify-start">
@@ -114,10 +122,10 @@ export default function Consultations() {
         </div>
         </div>
         </div>
-        <div className="flex justify-end items-center sm:px-25 px-5 pb-20">
+        <div className="flex justify-end items-center sm:px-25 px-5 pb-20 z-90">
           <div
             id="maps"
-            className="hidden md:block md:mt-25 lg:mt-0 shadow-xl scroll-mt-40"
+            className="hidden md:block md:mt-25 lg:mt-0 shadow-xl scroll-mt-40 z-50"
           >
             <iframe
               className="mx-auto rounded-lg"
@@ -137,10 +145,18 @@ export default function Consultations() {
       <div className="pt-20 bg-emerald-200/50 h-180 -mb-70">
       <div className="to-show animate-on-scroll">
         <div id="pour-qui" className="scroll-mt-30 flex flex-nowrap w-full pl-25 mb-5 z-90  text-lg">
-          <a href="#infos-pratiques">
-            <h2 className="rounded-l px-3 py-1 text-slate-300">Informations pratiques</h2>
+          <a href="#infos-pratiques" className="group hover:scale-104">
+            <h2 className="rounded-l px-3 py-1 text-slate-300 hover:text-slate-400/90">Informations pratiques</h2>
+            <hr
+              className="w-fit border-t
+              border-slate-400/90
+                -mt-1.5
+                mx-auto
+                group-hover:animate-[hr-fill_0.8s_cubic-bezier(0.4,0,0.2,1)_forwards]
+                group-hover:w-[82%]"
+            />
           </a>
-            <span className="px-3 py-1 text-slate-300">-</span>
+            {/* <span className="px-3 py-1 text-slate-300">-</span> */}
           <h2 className="rounded-r px-3 py-1 text-slate-500">Pour qui</h2>
         </div>
         <div className="sm:px-10 md:px-15 px-5 gap-x-10 grid grid-cols-[minmax(90px,3fr)_minmax(120px,4fr)] w-4/5 items-start justify-start mt-15">
